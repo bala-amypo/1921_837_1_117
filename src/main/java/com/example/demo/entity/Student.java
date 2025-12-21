07282315e7cf0478a8c56ed3863f303231b92468
bala-amypo <bala@amypo.in>
2025-12-21 18:52:17 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,58 +1,30 @@
 package com.example.demo.entity;
 
-import java.time.LocalDate;
+
+import jakarta.persistence.*;
+
+
+@Entity
 public class Student {
-    private long id;
-    private String name;
-    private String email;
-    private LocalDate dob;
-    private float cgpa;
-    public long getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public LocalDate getDob() {
-        return dob;
-    }
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-    public float getCgpa() {
-        return cgpa;
-    }
-    public void setCgpa(float cgpa) {
-        this.cgpa = cgpa;
-    }
-    public Student(int id, String name, String email, LocalDate dob, float cgpa) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.dob = dob;
-        this.cgpa = cgpa;
-    }
-    public Student() {
-    }
 
-    // public void setId(int id) { //Setting the data Id
-    //     this.id = id;
-    // }
-    // public int getId() { //Getting the data Id
-    //     return id;
-    // }
 
-    
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+
+
+private String name;
+private String email;
+
+
+public Long getId() { return id; }
+public void setId(Long id) { this.id = id; }
+
+
+public String getName() { return name; }
+public void setName(String name) { this.name = name; }
+
+
+public String getEmail() { return email; }
+public void setEmail(String email) { this.email = email; }
 }
