a95849fb6d13bf6571023471af716124c6384bff
bala-amypo <bala@amypo.in>
2025-12-27 08:23:19 +0000

    User commit

--- src/main/java/com/example/demo/entity/PolicyRule.java
+++ src/main/java/com/example/demo/entity/PolicyRule.java
@@ -1,22 +1,14 @@
-package com.example.demo.entity;
-
-
-import jakarta.persistence.*;
-
-
 @Entity
+@Getter
+@Setter
 public class PolicyRule {
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-
-@Column(unique = true)
-private String ruleCode;
-
-
-private String description;
-private String severity;
-private String conditionsJson;
-private Boolean active;
+    private String ruleCode;
+    private String description;
+    private String severity;
+    private String conditionsJson;
+    private Boolean active;
 }
