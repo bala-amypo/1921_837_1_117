b9f755e093c7b4914cc4503c95157cdefdbc7a0c
bala-amypo <bala@amypo.in>
2025-12-27 07:59:17 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/LoginEvent.java
@@ -0,0 +1,19 @@
+package com.example.demo.entity;
+
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Entity
+public class LoginEvent {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+private Long userId;
+private String ipAddress;
+private String location;
+private String deviceId;
+private String loginStatus;
+private LocalDateTime timestamp = LocalDateTime.now();
+}
---
+++ src/main/java/com/example/demo/entity/UserAccount.java
@@ -0,0 +1,31 @@
+package com.example.demo.entity;
+
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Entity
+public class UserAccount {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+
+
+@Column(unique = true)
+private String employeeId;
+
+
+@Column(unique = true)
+private String username;
+
+
+@Column(unique = true)
+private String email;
+
+
+private String password;
+private String role;
+private String status = "ACTIVE";
+private LocalDateTime createdAt = LocalDateTime.now();
+}
