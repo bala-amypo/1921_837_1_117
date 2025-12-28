64c307e10d436dee0a01e2e33d8f32bb465ab2e8
bala-amypo <bala@amypo.in>
2025-12-28 17:13:00 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/example/demo/security/JwtAuthenticationEntryPoint.java
@@ -6,12 +6,14 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import java.io.IOException;
 
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
     @Override
     public void commence(
             HttpServletRequest request,
             HttpServletResponse response,
             org.springframework.security.core.AuthenticationException authException)
             throws IOException {
+
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
     }
 }
